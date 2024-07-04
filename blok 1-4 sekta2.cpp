a77521f390e0ea9653d1f49e1eed2af7a422e7f8
piotrkrysik <147055503+piotrkrysik@users.noreply.github.com>
2024-07-04 13:52:16 +0200

    Add files via upload

--- blok 1-4 sekta2.cpp
+++ blok 1-4 sekta2.cpp
@@ -7,17 +7,24 @@ using namespace std;
 
 short* alokujTablice(short size)
 {
+    if (size <= 0) return nullptr;
     static short counter = 0;
     short* tablica = new short[size+1] {};
-    tablica[0] = size << 8| counter;
+    tablica[0] = counter << 8| size;
     counter++;
     return (tablica+1);
+}
+
+void dealokujTablice(short* tablica)
+{
+    delete[](tablica - 1);
+    tablica = nullptr;
 }
 
 void zapiszTablice(short* tablica, const char* nazwaPliku)
 {
     FILE* plik = fopen(nazwaPliku, "w+b");
-    if (plik == nullptr) {
+    if (plik == nullptr || tablica == nullptr) {
         cerr << "Błąd otwarcia pliku do zapisu\n";
         return;
     }
@@ -25,7 +32,8 @@ void zapiszTablice(short* tablica, const char* nazwaPliku)
     // Najpierw zapisujemy rozmiar tablicy i licznik
     short zlaczenie = tablica[-1];
     uint16_t rozmiar = zlaczenie;
-    rozmiar >>= 8;
+    short maska = 0xFF;
+    rozmiar = rozmiar & maska;
     cout << "zlaczenie: " << bitset<16>(zlaczenie) << "\n";
     cout << "rozmiar: " << bitset<16>(rozmiar) << "\nBez bitsetu: "<<rozmiar << "\n";
     fwrite(&zlaczenie, sizeof(short), 1, plik);
@@ -41,57 +49,110 @@ void zapiszTablice(short* tablica, const char* nazwaPliku)
 }
 
 
-void odczytajPlik(const char* nazwaPliku)
+short* odczytajPlik(const char* nazwaPliku)
 {
     FILE* plik = fopen(nazwaPliku, "r+b");
     if (plik == nullptr) {
         cerr << "Błąd otwarcia pliku do odczytu\n";
-        return;
+        return nullptr;
     }
     short zlaczenie;
     fread(&zlaczenie, sizeof(short), 1, plik);
     cout << "odczyt zlaczenia z pliku: " << bitset<16>(zlaczenie)<<"\n";
-    short czesc1 = (zlaczenie >> 8);
-    uint16_t maska = 0x00C8;
+    short czesc1 = zlaczenie;
+    uint16_t maska = 0xFF;
     short rozmiar = (czesc1 & maska);
     cout << "rozmiar z pliku: " << bitset<16>(rozmiar) << "\nBez bitsetu z pliku: " << rozmiar << "\n";
-    uint16_t licznik = zlaczenie & 0xFF;
 
-    cout << "Rozmiar: " << rozmiar << ", Licznik: " << licznik << endl;
-    short* tablica = new short[rozmiar];
+    cout << "Rozmiar: " << rozmiar << endl;
+    short* tablica = alokujTablice(rozmiar);
     fread(tablica, sizeof(short), rozmiar, plik);
 
-    // Wyświetlamy zawartość tablicy
-    for (int i = 0; i < rozmiar; i++)
-    {
-        cout << tablica[i] << ", ";
-    }
-    cout << endl;
+    //// Wyświetlamy zawartość tablicy
+    //for (int i = 0; i < rozmiar; i++)
+    //{
+    //    cout << tablica[i] << ", ";
+    //}
+    //cout << endl;
 
-    delete[] tablica;
+    //delete[] tablica;
 
     fclose(plik);
+
+    return tablica;
 }
 
-void odczytajRozmiarILicznik(short* tablica) {
+short odczytajRozmiar(short* tablica) {
     uint16_t zlaczenie = *(tablica - 1);
-    short size = zlaczenie >> 8;
-    uint16_t licznik = zlaczenie & 0xFF;
-    cout << "Rozmiar: " << size << ", Licznik: " << licznik << endl;
+    short size = zlaczenie;
+    size = zlaczenie & 0xFF;
+    
+    return size;
 }
 
+bool porownajBinarne(short *a, short* b)
+{
+    short rozmiarA = odczytajRozmiar(a);
+    short rozmiarB = odczytajRozmiar(b);
+
+    std::cout << " Porownanie rozmiarow tablic: " << rozmiarA << " " << rozmiarB;
+
+    if (rozmiarA == rozmiarB) {
+
+        return (memcmp(a, b, rozmiarA * sizeof(short)) == 0);
+    }
+    else return false;
+}
+
+void generujTablice(short* tab)
+{
+    for (int i = 0; i < odczytajRozmiar(tab); i++) {
+        tab[i] = rand() % 100;
+    }
+}
+
+
+void odczytajTablice(short* tab)
+{
+    for (int i = 0; i < odczytajRozmiar(tab); i++) {
+        std::cout << " " << tab[i];
+    }
+}
 
 int main()
 {
-    short size = 200;
-    short* wskaznikNaTablice = alokujTablice(size);
-    short* wskaznikNaTablice1 = alokujTablice(size);
-    //cout << "16/8: " << bitset<16>(wskaznikNaTablice1[-1]) << "\n";
-    zapiszTablice(wskaznikNaTablice, "faddtek.txt");
-    zapiszTablice(wskaznikNaTablice1, "maciek.txt");
-    odczytajPlik("faddtek.txt");
-    odczytajPlik("maciek.txt");
-    delete[](wskaznikNaTablice - 1);
-    delete[](wskaznikNaTablice1 - 1);
+    srand(time(NULL));
 
+    short size = 20;
+    short* wskaznikNaTablice = alokujTablice(size);
+    short* wskaznikNaTablice1 = alokujTablice(size+ 10); 
+    short* wskaznikNaTablice2 = alokujTablice(size + 20);
+
+    generujTablice(wskaznikNaTablice);
+    generujTablice(wskaznikNaTablice1);
+    generujTablice(wskaznikNaTablice2);
+    std::cout << " Odczytywanie tablic:\n";
+    odczytajTablice(wskaznikNaTablice); std::cout << "\n";
+    odczytajTablice(wskaznikNaTablice1); std::cout << "\n";
+    odczytajTablice(wskaznikNaTablice2); std::cout << "\n";
+
+    //cout << "16/8: " << bitset<16>(wskaznikNaTablice1[-1]) << "\n";
+    std::cout << " ZAPIS:\n "; 
+    zapiszTablice(wskaznikNaTablice2, "kutek.txt");
+    std::cout << " ODCZYT:\n ";
+    short* wskaznikNaTablice3 = odczytajPlik("kutek.txt");
+
+    if (porownajBinarne(wskaznikNaTablice3, wskaznikNaTablice2))
+    {
+        std::cout << "\nTakie same" << "\n";
+    }
+    else
+    {
+        std::cout << "Nie takie same" << "\n";
+    }
+
+    dealokujTablice(wskaznikNaTablice);
+    dealokujTablice(wskaznikNaTablice1);
+    dealokujTablice(wskaznikNaTablice2);
+    dealokujTablice(wskaznikNaTablice3);
 }
